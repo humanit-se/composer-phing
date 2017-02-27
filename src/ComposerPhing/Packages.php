66387022b44573f6beb4db12781f7e0894630326
Jamie <jamie.telin@gmail.com>
2017-02-27 14:44:18 +0100

    Add log messages

--- src/ComposerPhing/Packages.php
+++ src/ComposerPhing/Packages.php
@@ -2,6 +2,9 @@
 namespace ComposerPhing;
 
 use Composer\Script\Event;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use \Bramus\Monolog\Formatter\ColoredLineFormatter;
 
 class Packages
 {
@@ -13,22 +16,28 @@ class Packages
 
         $args = $event->getArguments();
 
+        $log = new Logger(__NAMESPACE__);
+        $handler = new StreamHandler('php://stderr', Logger::INFO);
+        $handler->setFormatter(new ColoredLineFormatter(null, "[%level_name%] %message%", null, true));
+        $log->pushHandler($handler);
+
         $repositoryManager = $composer->getRepositoryManager();
         $installationManager = $composer->getInstallationManager();
         $localRepository = $repositoryManager->getLocalRepository();
 
         $packages = $localRepository->getPackages();
+        $log->info("Found (" . count($packages) . ") packages");
 
-        $task = '';
-        if (isset($args[0])) {
-          $task = $args[0];
-        }
+        $task = isset($args[0]) ? $args[0] : '';
 
         foreach ($packages as $package) {
             $installPath = $installationManager->getInstallPath($package);
+            $command = "phing -buildfile {$installPath}/build.xml {$task}";
             if (is_file("{$installPath}/build.xml")) {
-                var_dump("Executing; ant -buildfile {$installPath}/build.xml {$task}");
-                shell_exec("ant -buildfile {$installPath}/build.xml {$task}");
+                $log->info("Package; {$package} \n\t Executing;\t{$command}");
+                shell_exec($command);
+            } else {
+                $log->notice("Package; {$package} has no build.xml");
             }
         }
     }
